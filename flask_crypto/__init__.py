fcb3c27aa4ec8030bb7faab3aa8e493bd5bdaa90
princekrroshan01 <princekrroshan01@gmail.com>
2020-05-04 15:15:41 +0530

    fixed some typo

--- flask_crypto/__init__.py
+++ flask_crypto/__init__.py
@@ -1,6 +1,3 @@
 from .flask_crypto import Crypto
 
-from flask import Flask 
-app=Flask(__name__)
-crypto = Crypto(app)
-print(crypto)
+
--- flask_crypto/config.py
+++ flask_crypto/config.py
@@ -2,9 +2,7 @@
 Class which sets configuration parameter for the extension and helps in manupulation of configuration parameter. 
 
 """
+
+
 class Config:
-	pass
-	
-	
-
-
+    pass
